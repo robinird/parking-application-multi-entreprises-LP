b2fc7862cf8565b3a62c98da8d48a05d17996dca
robinird <robin.rouillard@outlook.fr>
2026-08-23 19:37:32 +0200

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import './globals.css'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'ParkFlow - Gestion du parking d\'entreprise & Travail Hybride',
+  description: 'Fin des fichiers Excel ingérables. Offrez une attribution de parking équitable et fluide à vos équipes en 2 minutes chrono.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  )
+}
