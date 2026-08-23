782839af338d89f6872af378641f6dd483f31ffb
robinird <robin.rouillard@outlook.fr>
2026-08-23 20:24:35 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'ParkFlow - Gestion du parking d\'entreprise & Travail Hybride',
+  title: 'TechCorp Parking - Gestion du parking d\'entreprise & Travail Hybride',
   description: 'Fin des fichiers Excel ingérables. Offrez une attribution de parking équitable et fluide à vos équipes en 2 minutes chrono.',
 }
 
